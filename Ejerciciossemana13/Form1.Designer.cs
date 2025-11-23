ffc3c10484c822a5b039cd2d710525513f7b61fc
EtherealWeb5167 <josalgado@uamv.edu.ni>
2025-11-23 09:06:17 -0600

    Add project files.

---
+++ Ejerciciossemana13/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Ejerciciossemana13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.btnGenerarEjercicio1 = new System.Windows.Forms.Button();
+            this.tbBuscarEjercicio1 = new System.Windows.Forms.TextBox();
+            this.lblEjercicio1 = new System.Windows.Forms.Label();
+            this.btnBuscarEjercicio1 = new System.Windows.Forms.Button();
+            this.lblResultadoEjercicio1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(784, 434);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lblResultadoEjercicio1);
+            this.tabPage1.Controls.Add(this.btnBuscarEjercicio1);
+            this.tabPage1.Controls.Add(this.lblEjercicio1);
+            this.tabPage1.Controls.Add(this.tbBuscarEjercicio1);
+            this.tabPage1.Controls.Add(this.btnGenerarEjercicio1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(776, 405);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ej1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(776, 405);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Ej2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(776, 405);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Ej3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(776, 405);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Ej4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 25);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(776, 405);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Ej5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 25);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(776, 405);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Ej6";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerarEjercicio1
+            // 
+            this.btnGenerarEjercicio1.Location = new System.Drawing.Point(175, 33);
+            this.btnGenerarEjercicio1.Name = "btnGenerarEjercicio1";
+            this.btnGenerarEjercicio1.Size = new System.Drawing.Size(134, 23);
+            this.btnGenerarEjercicio1.TabIndex = 0;
+            this.btnGenerarEjercicio1.Text = "Generar";
+            this.btnGenerarEjercicio1.UseVisualStyleBackColor = true;
+            this.btnGenerarEjercicio1.Click += new System.EventHandler(this.btnBuscarEjercicio1_Click);
+            // 
+            // tbBuscarEjercicio1
+            // 
+            this.tbBuscarEjercicio1.Location = new System.Drawing.Point(41, 33);
+            this.tbBuscarEjercicio1.Name = "tbBuscarEjercicio1";
+            this.tbBuscarEjercicio1.Size = new System.Drawing.Size(100, 22);
+            this.tbBuscarEjercicio1.TabIndex = 1;
+            // 
+            // lblEjercicio1
+            // 
+            this.lblEjercicio1.AutoSize = true;
+            this.lblEjercicio1.Location = new System.Drawing.Point(38, 94);
+            this.lblEjercicio1.Name = "lblEjercicio1";
+            this.lblEjercicio1.Size = new System.Drawing.Size(73, 16);
+            this.lblEjercicio1.TabIndex = 2;
+            this.lblEjercicio1.Text = "esperando";
+            // 
+            // btnBuscarEjercicio1
+            // 
+            this.btnBuscarEjercicio1.Location = new System.Drawing.Point(175, 62);
+            this.btnBuscarEjercicio1.Name = "btnBuscarEjercicio1";
+            this.btnBuscarEjercicio1.Size = new System.Drawing.Size(134, 23);
+            this.btnBuscarEjercicio1.TabIndex = 3;
+            this.btnBuscarEjercicio1.Text = "Buscar";
+            this.btnBuscarEjercicio1.UseVisualStyleBackColor = true;
+            this.btnBuscarEjercicio1.Click += new System.EventHandler(this.btnBuscarEjercicio1_Click_1);
+            // 
+            // lblResultadoEjercicio1
+            // 
+            this.lblResultadoEjercicio1.AutoSize = true;
+            this.lblResultadoEjercicio1.Location = new System.Drawing.Point(330, 69);
+            this.lblResultadoEjercicio1.Name = "lblResultadoEjercicio1";
+            this.lblResultadoEjercicio1.Size = new System.Drawing.Size(73, 16);
+            this.lblResultadoEjercicio1.TabIndex = 4;
+            this.lblResultadoEjercicio1.Text = "esperando";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.Button btnGenerarEjercicio1;
+        private System.Windows.Forms.Label lblEjercicio1;
+        private System.Windows.Forms.TextBox tbBuscarEjercicio1;
+        private System.Windows.Forms.Button btnBuscarEjercicio1;
+        private System.Windows.Forms.Label lblResultadoEjercicio1;
+    }
+}
+
---
+++ Ejerciciossemana13/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ejerciciossemana13
+{
+    public partial class Form1 : Form
+    {
+
+        int[] numeros = new int[20];
+        bool listagenerada = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscarEjercicio1_Click(object sender, EventArgs e)
+        {
+            Random aleatorio = new Random();
+            string textoLista = "";
+
+            for (int i = 0; i < 20; i++)
+            {
+                numeros[i] = aleatorio.Next(1, 100);
+                textoLista += numeros[i] + ", ";
+            }
+
+            listagenerada = true;
+
+            
+            lblEjercicio1.Text = textoLista;
+
+        }
+
+        private void btnBuscarEjercicio1_Click_1(object sender, EventArgs e)
+        {
+            if (!listagenerada)
+            {
+                MessageBox.Show("Primero debes presionar el boton Generar");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(tbBuscarEjercicio1.Text))
+            {
+                MessageBox.Show("Escribe un numero primero.");
+                return;
+            }
+
+            int numeroBuscado;
+            if (!int.TryParse(tbBuscarEjercicio1.Text, out numeroBuscado))
+            {
+                lblResultadoEjercicio1.Text = "Error: Ingresa solo numeros.";
+                return;
+            }
+
+            bool encontrado = false;
+            int posicion = -1;
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (numeros[i] == numeroBuscado)
+                {
+                    encontrado = true;
+                    posicion = i;
+                    break;
+                }
+            }
+
+            if (encontrado)
+            {
+                lblResultadoEjercicio1.Text = "Encontrado en la posicion: " + (posicion);
+            }
+            else
+            {
+                lblResultadoEjercicio1.Text = "El numero " + numeroBuscado + " no existe en la lista.";
+            }
+        }
+    }
+}
